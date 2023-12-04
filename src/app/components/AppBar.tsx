f864f0de60d2763fdd2a10be6b535b8477f27ce2
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2023-12-04 23:59:33 +0200

    add AppBar component

---
+++ src/app/components/AppBar.tsx
@@ -0,0 +1,7 @@
+export default function AppBar() {
+  return (
+    <header className="bg-lime-700 h-12 flex justify-center items-center">
+      <div className="">Irregular verbs learning app</div>
+    </header>
+  );
+}
