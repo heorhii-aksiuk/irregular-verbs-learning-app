9a64acda4889f0d46800861b39710b5b50670792
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2023-12-05 20:08:22 +0200

    add navigation

--- src/app/components/AppBar.tsx
+++ src/app/components/AppBar.tsx
@@ -1,7 +1,48 @@
+import Link from 'next/link';
+
+interface Page {
+  id: number;
+  path: string;
+  title: string;
+}
+
+const pages: Page[] = [
+  {
+    id: 1,
+    path: '/',
+    title: 'Test',
+  },
+  {
+    id: 2,
+    path: '/words',
+    title: 'Words',
+  },
+  {
+    id: 3,
+    path: '/stat',
+    title: 'Stat',
+  },
+];
+
 export default function AppBar() {
   return (
     <header className="bg-gray-400 h-12 flex justify-center items-center">
-      <div className="">Irregular verbs learning app</div>
+      <div className="flex">
+        <p className="mr-6">
+          <Link href={pages[0].path}>Irregular verbs learning app</Link>
+        </p>
+        <nav>
+          <ul className="flex">
+            {pages.map((page) => (
+              <li key={page.id} className="mr-6">
+                <Link className="hover:text-blue-600" href={page.path}>
+                  {page.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
--- src/app/statistic/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Statistic() {
-  return <div>Statistic</div>;
-}
